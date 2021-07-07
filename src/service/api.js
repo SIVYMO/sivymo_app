83a28f849fe4ff1c29fc4e4fa849c95fc91ee278
Hector Saldaña Espinoza <61708286+HectorSaldes@users.noreply.github.com>
2021-07-07 18:37:06 -0500

    deploy 4

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:8080/novopatent-api",
+    baseURL: "https://sivymoapi.herokuapp.com/novopatent-api",
     headers: {
         "Content-type": "application/json",
         "Access-Control-Allow-Origin": "*",
