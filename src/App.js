7adab709677c14586cb797e90bc18c0c04ee3d06
SIVYMO <sivymo.norepley@gmail.com>
2022-12-21 11:09:45 -0600

    Add correct routes

--- src/App.js
+++ src/App.js
@@ -21,11 +21,11 @@ export default function App() {
         <Router>
             <Switch>
                 <Route path="/" exact component={Login} />
-                <Route path="/sivymo_app/inicio" component={HomePage} />
-                <Route path="/sivymo_app/patentes" component={PatentPage} />
-                <Route path="/sivymo_app/marcas" component={BrandPage} />
-                <Route path="/sivymo_app/expedientes" component={InformationPage} />
-                <Route path="/sivymo_app/perfil" component={ProfilePage} />
+                <Route path="/inicio" component={HomePage} />
+                <Route path="/patentes" component={PatentPage} />
+                <Route path="/marcas" component={BrandPage} />
+                <Route path="/expedientes" component={InformationPage} />
+                <Route path="/perfil" component={ProfilePage} />
                 <Route component={ErrorPage} />
             </Switch>
         </Router>
