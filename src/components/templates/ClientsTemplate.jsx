f1ab86c186fb13bb75f24fdee5e0c3647f4a4827
SIVYMO <sivymo.norepley@gmail.com>
2023-01-23 10:30:54 -0600

    Improve upload files

--- src/components/templates/ClientsTemplate.jsx
+++ src/components/templates/ClientsTemplate.jsx
@@ -1,199 +1,256 @@
-import React, {useState, useRef, useEffect} from "react";
-import {Toast} from "primereact/toast";
-import {InputTextarea} from 'primereact/inputtextarea';
+import React, { useState, useRef, useEffect } from "react";
+import { Toast } from "primereact/toast";
+import { InputTextarea } from "primereact/inputtextarea";
 import ClienteService from "../../service/ClienteService";
 import HistorialService from "../../service/HistorialService";
-import {BreadCrumb} from "primereact/breadcrumb";
+import { BreadCrumb } from "primereact/breadcrumb";
 import {
-    txtSmsLoading,
-    txtMessageErrorGeneral,
-    txtMessageClientsSaved,
-    txtTitleClients,
-    txtLastUpdateClients,
+  txtSmsLoading,
+  txtMessageErrorGeneral,
+  txtMessageClientsSaved,
+  txtTitleClients,
+  txtLastUpdateClients,
 } from "../../utils/Strings";
 import moment from "moment";
 import "moment/locale/es";
-import {Button} from "primereact/button";
+import { Button } from "primereact/button";
 
 moment.locale("es");
 
 export default function ClientsTemplate() {
-    const [resume, setResume] = useState({});
-    const [allClients, setAllClients] = useState([]);
-    const [clients, setClients] = useState([]);
-    const [allClientsText, setAllClientsText] = useState("");
-    const [clientsText, setClientsText] = useState("");
+  const [resume, setResume] = useState({});
+  const [allClients, setAllClients] = useState([]);
+  const [clients, setClients] = useState([]);
+  const [allClientsText, setAllClientsText] = useState("");
+  const [clientsText, setClientsText] = useState("");
 
-    const toast = useRef(null);
+  const toast = useRef(null);
 
-    useEffect(() => {
-        getResume();
-    }, []);
+  useEffect(() => {
+    getResume();
+  }, []);
 
-    const getResume = () => {
-        setResume(JSON.parse(localStorage.getItem("resume")));
-    };
+  const getResume = () => {
+    setResume(JSON.parse(localStorage.getItem("resume")));
+  };
 
-    const clearAndFilterData = (data = []) => {
-        let newData = [];
-        data.forEach((item) => {
-            item = item.trim();
-            if (item.length > 2) newData.push(item);
-        });
-        return newData;
-    };
+  const clearAndFilterData = async (data = []) => {
+    let newData = [];
+    for await (let item of data) {
+      item = item.trim();
+      if (item.length > 2) newData.push(item);
+    }
+    return newData;
+  };
 
-    const saveAllClients = () => {
-        showMessageloading()
-        const data = clearAndFilterData(allClients)
-        const middleBig1 = data.splice(0, data.length / 2);
-        const middleTiny1 = middleBig1.splice(0, middleBig1.length / 2);
-        const middleTiny2 = middleBig1.splice(0, middleBig1.length);
-        const middleBig2 = data.splice(0, data.length);
-        const middleTiny3 = middleBig2.splice(0, middleBig2.length / 2);
-        const middleTiny4 = middleBig2.splice(0, middleBig2.length);
-        Promise.all([
-                ClienteService.insertAll(middleTiny1),
-                ClienteService.insertAll(middleTiny2),
-                ClienteService.insertAll(middleTiny3),
-                ClienteService.insertAll(middleTiny4),
-            ]
-        ).then((res) => {
-            console.log(res);
-            if (res[0].data && res[1].data && res[2].data && res[3].data) {
-                clearMessageLoading()
+  const saveAllClients = async () => {
+    showMessageloading();
+    const data = await clearAndFilterData(allClients);
+    const middleBig1 = data.splice(0, data.length / 2);
+    const middleTiny1 = middleBig1.splice(0, middleBig1.length / 2);
+    const middleTiny2 = middleBig1.splice(0, middleBig1.length);
+    const middleBig2 = data.splice(0, data.length);
+    const middleTiny3 = middleBig2.splice(0, middleBig2.length / 2);
+    const middleTiny4 = middleBig2.splice(0, middleBig2.length);
+    ClienteService.insertAll(middleTiny1)
+      .then((res) => {
+        if (res.data) {
+          Promise.all([
+            ClienteService.insertOne(middleTiny2),
+            ClienteService.insertOne(middleTiny3),
+            ClienteService.insertOne(middleTiny4),
+          ])
+            .then((res) => {
+              if (res[0].data && res[1].data && res[2].data) {
+                clearMessageLoading();
                 saveHistory();
-                clearAllClients()
-                showMessage(txtMessageClientsSaved)
-            } else showMessage(txtMessageErrorGeneral);
-        }).catch((err) => {
-            clearMessageLoading()
-            console.error(err);
-            showMessage(txtMessageErrorGeneral);
-        })
-    };
-
-    const saveClients = () => {
-        showMessageloading()
-        const data = clearAndFilterData(clients)
-        ClienteService.insertOne(data)
-            .then((response) => {
-                clearMessageLoading()
-                if (response) {
-                    saveHistory();
-                    clearClients();
-                    showMessage(txtMessageClientsSaved)
-                } else {
-                    showMessage(txtMessageErrorGeneral)
-                }
+                clearAllClients();
+                showMessage(txtMessageClientsSaved);
+              } else showMessage(txtMessageErrorGeneral);
             })
-            .catch((error) => {
-                clearMessageLoading()
-                console.error(error);
-                showMessage(txtMessageErrorGeneral);
-            })
-    };
+            .catch((err) => {
+              clearMessageLoading();
+              console.error(err);
+              showMessage(txtMessageErrorGeneral);
+            });
+        } else showMessage(txtMessageErrorGeneral);
+      })
+      .catch((err) => {
+        clearMessageLoading();
+        console.error(err);
+        showMessage(txtMessageErrorGeneral);
+      });
+  };
 
-    const clearAllClients = () => {
-        setAllClients([]);
-        setAllClientsText("");
-        showMessage({type: 'info', title: 'Se han limpiado todos expedientes', description: 'Limpiado'});
-    };
-    const clearClients = () => {
-        setClients([]);
-        setClientsText("");
-        showMessage({type: 'info', title: 'Se han limpiado los expedientes', description: 'Limpiado'});
-    }
+  const saveClients = () => {
+    showMessageloading();
+    const data = clearAndFilterData(clients);
+    ClienteService.insertOne(data)
+      .then((response) => {
+        clearMessageLoading();
+        if (response) {
+          saveHistory();
+          clearClients();
+          showMessage(txtMessageClientsSaved);
+        } else {
+          showMessage(txtMessageErrorGeneral);
+        }
+      })
+      .catch((error) => {
+        clearMessageLoading();
+        console.error(error);
+        showMessage(txtMessageErrorGeneral);
+      });
+  };
 
-    const clearMessageLoading = () => {
-        toast.current.clear();
-    };
+  const clearAllClients = () => {
+    setAllClients([]);
+    setAllClientsText("");
+    showMessage({
+      type: "info",
+      title: "Se han limpiado todos expedientes",
+      description: "Limpiado",
+    });
+  };
+  const clearClients = () => {
+    setClients([]);
+    setClientsText("");
+    showMessage({
+      type: "info",
+      title: "Se han limpiado los expedientes",
+      description: "Limpiado",
+    });
+  };
 
-    const showMessageloading = () => {
-        toast.current.show({
-            severity: "info",
-            sticky: true,
-            content: (
-                <div className="p-flex p-flex-column" style={{flex: "1"}}>
-                    <div className="p-text-center">
-                        <i className="pi pi-spin pi-spinner" style={{fontSize: "3em"}}></i>
-                        <h4>{txtSmsLoading[0]}</h4>
-                        <p>{txtSmsLoading[1]}</p>
-                    </div>
+  const clearMessageLoading = () => {
+    toast.current.clear();
+  };
+
+  const showMessageloading = () => {
+    toast.current.show({
+      severity: "info",
+      sticky: true,
+      content: (
+        <div className="p-flex p-flex-column" style={{ flex: "1" }}>
+          <div className="p-text-center">
+            <i
+              className="pi pi-spin pi-spinner"
+              style={{ fontSize: "3em" }}
+            ></i>
+            <h4>{txtSmsLoading[0]}</h4>
+            <p>{txtSmsLoading[1]}</p>
+          </div>
+        </div>
+      ),
+    });
+  };
+
+  const saveHistory = () => {
+    HistorialService.insertOne(3);
+  };
+
+  const showMessage = ({ type, title, description }) => {
+    toast.current.show({
+      severity: type,
+      summary: title,
+      detail: description,
+      life: 3000,
+    });
+  };
+
+  return (
+    <>
+      <BreadCrumb
+        model={[{ label: txtTitleClients }]}
+        home={{ icon: "pi pi-home" }}
+      />
+      <Toast ref={toast} />
+      <div className="p-grid">
+        <div className="p-col p-p-3">
+          <h1>{txtTitleClients}</h1>
+          <div>
+            {txtLastUpdateClients}
+            {moment(resume.ultimaModificacionClientes).format("LLLL")}
+          </div>
+          <div className="p-grid p-mt-3">
+            <div className="p-col-12 p-sm-6">
+              <div className="p-grid">
+                <div className="p-col-12 p-sm-8">
+                  <h2>
+                    Guardar todos los expedientes :{" "}
+                    <span>{allClients.length}</span>
+                  </h2>
                 </div>
-            ),
-        });
-    };
-
-    const saveHistory = () => {
-        HistorialService.insertOne(3)
-    }
-
-    const showMessage = ({type, title, description}) => {
-        toast.current.show({severity: type, summary: title, detail: description, life: 3000,});
-    };
-
-    return (
-        <>
-            <BreadCrumb
-                model={[{label: txtTitleClients}]}
-                home={{icon: "pi pi-home"}}
-            />
-            <Toast ref={toast}/>
-            <div className="p-grid">
-                <div className="p-col p-p-3">
-                    <h1>{txtTitleClients}</h1>
-                    <div>
-                        {txtLastUpdateClients}
-                        {moment(resume.ultimaModificacionClientes).format("LLLL")}
-                    </div>
-                    <div className='p-grid p-mt-3'>
-                        <div className='p-col-12 p-sm-6'>
-                            <div className="p-grid">
-                                <div className="p-col-12 p-sm-8">
-                                    <h2>Guardar todos los expedientes : <span>{allClients.length}</span></h2>
-                                </div>
-                                <div className="p-col-12 p-sm-4 p-mt-0 p-mt-sm-2">
-                                    <Button type="button" icon="pi pi-times" label='Limpiar'
-                                            className="p-button-info p-mr-1" onClick={clearAllClients}
-                                            disabled={allClients.length <= 0}/>
-                                    <Button type="button" icon="pi pi-save" label='Guardar'
-                                            className="p-button-success"
-                                            onClick={saveAllClients}
-                                            disabled={allClients.length <= 0}/>
-                                </div>
-                            </div>
-                            <InputTextarea value={allClientsText} rows={20} autoResize style={{width: '100%'}}
-                                           onChange={(e) => {
-                                               setAllClientsText(e.target.value)
-                                               setAllClients(e.target.value.split("\n"))
-                                           }}/>
-                        </div>
-                        <div className='p-col-12 p-sm-6'>
-                            <div className="p-grid">
-                                <div className="p-col">
-                                    <h2>Añadir más expedientes : <span>{clients.length}</span></h2>
-                                </div>
-                                <div className="p-col-12 p-sm-4 p-mt-0 p-mt-sm-2">
-                                    <Button type="button" icon="pi pi-times" label='Limpiar'
-                                            className="p-button-info p-mr-1" onClick={clearClients}
-                                            disabled={clients.length <= 0}/>
-                                    <Button type="button" icon="pi pi-save" label='Guardar'
-                                            className="p-button-warning"
-                                            onClick={saveClients}
-                                            disabled={clients.length <= 0}/>
-                                </div>
-                            </div>
-                            <InputTextarea value={clientsText} rows={20} autoResize style={{width: '100%'}}
-                                           onChange={(e) => {
-                                               setClientsText(e.target.value)
-                                               setClients(e.target.value.split("\n"))
-                                           }}/>
-                        </div>
-                    </div>
+                <div className="p-col-12 p-sm-4 p-mt-0 p-mt-sm-2">
+                  <Button
+                    type="button"
+                    icon="pi pi-times"
+                    label="Limpiar"
+                    className="p-button-info p-mr-1"
+                    onClick={clearAllClients}
+                    disabled={allClients.length <= 0}
+                  />
+                  <Button
+                    type="button"
+                    icon="pi pi-save"
+                    label="Guardar"
+                    className="p-button-success"
+                    onClick={saveAllClients}
+                    disabled={allClients.length <= 0}
+                  />
                 </div>
+              </div>
+              <InputTextarea
+                value={allClientsText}
+                rows={20}
+                autoResize
+                style={{ width: "100%" }}
+                onChange={(e) => {
+                  setAllClientsText(e.target.value);
+                  setAllClients(e.target.value.split("\n"));
+                }}
+              />
             </div>
-
-        </>
-    );
+            <div className="p-col-12 p-sm-6">
+              <div className="p-grid">
+                <div className="p-col">
+                  <h2>
+                    Añadir más expedientes : <span>{clients.length}</span>
+                  </h2>
+                </div>
+                <div className="p-col-12 p-sm-4 p-mt-0 p-mt-sm-2">
+                  <Button
+                    type="button"
+                    icon="pi pi-times"
+                    label="Limpiar"
+                    className="p-button-info p-mr-1"
+                    onClick={clearClients}
+                    disabled={clients.length <= 0}
+                  />
+                  <Button
+                    type="button"
+                    icon="pi pi-save"
+                    label="Guardar"
+                    className="p-button-warning"
+                    onClick={saveClients}
+                    disabled={clients.length <= 0}
+                  />
+                </div>
+              </div>
+              <InputTextarea
+                value={clientsText}
+                rows={20}
+                autoResize
+                style={{ width: "100%" }}
+                onChange={(e) => {
+                  setClientsText(e.target.value);
+                  setClients(e.target.value.split("\n"));
+                }}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
